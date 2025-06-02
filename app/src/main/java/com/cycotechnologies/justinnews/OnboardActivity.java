761a7d7e5f2124f5c6220571d997030b2bc99735
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2025-06-02 09:03:02 +0530

    signup Screen layout design files

---
+++ app/src/main/java/com/cycotechnologies/justinnews/OnboardActivity.java
@@ -0,0 +1,42 @@
+package com.cycotechnologies.justinnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OnboardActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_onboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnLogin = findViewById(R.id.gsignupbtn);
+        btnSignup = findViewById(R.id.signupBtn);
+
+        btnLogin.setOnClickListener(v->{
+            Intent loginIntent = new Intent(OnboardActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+        });
+
+        btnSignup.setOnClickListener(v->{
+            Intent signupIntent = new Intent(OnboardActivity.this, SignupActivity.class);
+            startActivity(signupIntent);
+        });
+        
+    }
+}
